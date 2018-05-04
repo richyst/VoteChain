8a26242d1738d26667f752d57bd852314e018dab
PabloEnriquez <12pable34@gmail.com>
2018-05-04 13:31:22 -0500

    Aux. Cambios para revertir a estado anterior

--- basic-network/generate.sh
+++ basic-network/generate.sh
@@ -9,32 +9,32 @@ export FABRIC_CFG_PATH=${PWD}
 CHANNEL_NAME=mychannel
 
 # remove previous crypto material and config transactions
-rm -fr config/*
-rm -fr crypto-config/*
+# rm -fr config/*
+# rm -fr crypto-config/*
 
 # generate crypto material
-cryptogen generate --config=./crypto-config.yaml
+./fabric-samples/bin/cryptogen generate --config=./crypto-config.yaml
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate crypto material..."
   exit 1
 fi
 
 # generate genesis block for orderer
-configtxgen -profile OneOrgOrdererGenesis -outputBlock ./config/genesis.block
+./fabric-samples/bin/configtxgen -profile OneOrgOrdererGenesis -outputBlock ./config/genesis.block
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate orderer genesis block..."
   exit 1
 fi
 
 # generate channel configuration transaction
-configtxgen -profile OneOrgChannel -outputCreateChannelTx ./config/channel.tx -channelID $CHANNEL_NAME
+./fabric-samples/bin/configtxgen -profile OneOrgChannel -outputCreateChannelTx ./config/channel.tx -channelID $CHANNEL_NAME
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate channel configuration transaction..."
   exit 1
 fi
 
 # generate anchor peer transaction
-configtxgen -profile OneOrgChannel -outputAnchorPeersUpdate ./config/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+./fabric-samples/bin/configtxgen -profile OneOrgChannel -outputAnchorPeersUpdate ./config/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate anchor peer update for Org1MSP..."
   exit 1
--- basic-network/start.sh
+++ basic-network/start.sh
@@ -26,4 +26,5 @@ docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/h
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
 
 # Join peer0.org1.example.com to the channel.
-# docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer1.org1.example.com peer channel join -b mychannel.block
+# docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp"
+# -e "CORE_PEER_ADDRESS=peer1.org1.example.com:7051" peer1.org1.example.com peer channel join -b mychannel.block
